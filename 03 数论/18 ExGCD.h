1fa40cb988945904c069f0b57c26735975261403
AINgray <1377292134@qq.com>
2023-11-07 22:20:35 +0800

    20231107

--- 03 数论/18 ExGCD.h
+++ 03 数论/18 ExGCD.h
@@ -1,5 +1,11 @@
 // a.inv (mod p)
 // 等价于ax+py==1的x
+int inv(int a, int p)
+{
+	int x, y;
+	exgcd(a, p, x, y);
+	return (p + x % p) % p;
+}
 
 // a * x + b * y == gcd(a, b) == g
 ll ExGCD(ll a, ll b, ll &x, ll &y)
